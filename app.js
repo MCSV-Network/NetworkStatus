ae277f0342b93ec2fe62e9e1cce600efb2a00fa6
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-28 00:49:18 +0900

    pc死ぬ前にcommit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+	try {
+	  const response = await axios.get('http://192.168.1.101:1919'); // ステータスを監視するURL
+	  const status = response.status;
+	  res.send(`Status: ${status}`);
+	} catch (error) {
+	  res.send('Error: Unable to fetch status');
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Listening localhost:${port}`)
+})
